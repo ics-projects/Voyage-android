9d0fa7eb0e8666b630d1267b99528a85b1b19b4d
Your Name <you@example.com>
2019-05-20 10:45:53 +0300

    Login feature

---
+++ app/src/main/java/com/example/real_estate_app_prototype/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.real_estate_app_prototype;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements NavigationHost {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new LoginFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/real_estate_app_prototype/WelcomePageViewModel.java
@@ -0,0 +1,7 @@
+package com.example.real_estate_app_prototype;
+
+import android.arch.lifecycle.ViewModel;
+
+public class WelcomePageViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
