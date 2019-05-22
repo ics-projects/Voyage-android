5bf019ac3054477d6471410298be9bf26cf5f2c3
SamChess <38308778+SamChess@users.noreply.github.com>
2019-05-22 12:53:49 +0300

    Login feature (#4)
    * Login feature
    
    * Remove unneded files and added .gitignore
    
    * Rename project

---
+++ app/src/main/java/com/example/voyage/WelcomePageFragment.java
@@ -0,0 +1,49 @@
+package com.example.voyage;
+
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WelcomePageFragment extends Fragment {
+
+    private WelcomePageViewModel mViewModel;
+
+    public static WelcomePageFragment newInstance() {
+        return new WelcomePageFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.welcome_page_fragment, container, false);
+
+        // Set up the toolbar
+        setUpToolbar(view);
+
+        return view;
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(WelcomePageViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
