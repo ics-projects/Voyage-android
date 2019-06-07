9eb14cb8817b06813b7acec9c592a911ff06e230
unknown <fcbsammymulwa@gmail.com>
2019-06-07 15:31:47 +0300

    progressDialogAdd

--- app/src/main/java/com/example/voyage/MainActivity.java
+++ app/src/main/java/com/example/voyage/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.voyage;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -57,17 +58,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void openWebPage(String url) {
-        Uri webpage = Uri.parse(url);
-        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
+//    public void openWebPage(String url) {
+//        Uri webpage = Uri.parse(url);
+//        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+//    }
 
     public void userLogin() {
         String email = emailinput.getText().toString();
         String password = pwdinput.getText().toString();
+
+        // display a progress dialog
+        final ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.setCancelable(false); // set cancelable to false
+        progressDialog.setMessage("Please Wait"); // set message
+        progressDialog.show(); // show progress dialog
 
         GetData service = VoyageClient.getRetrofitInstance().create(GetData.class);
 
@@ -79,15 +86,19 @@ public class MainActivity extends AppCompatActivity {
                 if (response.code() == 200) {
                     Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                    progressDialog.dismiss(); //dismiss progress dialog
                     startActivity(intent);
                 } else {
                     Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss(); //dismiss progress dialog
+
                 }
             }
 
             @Override
             public void onFailure(Call<LoginResponse> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "Oops, unable to login.", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss(); //dismiss progress dialog
             }
         });
     }
