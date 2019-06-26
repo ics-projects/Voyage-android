64687916cbc2ce3966fd178c3b3edc24f95612b6
johnngugi <johnngugi777@gmail.com>
2019-06-26 09:29:35 +0300

    Change date picker logic

--- app/src/main/java/com/example/voyage/SearchActivity.java
+++ app/src/main/java/com/example/voyage/SearchActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.text.SimpleDateFormat;
@@ -18,10 +17,13 @@ import java.util.Locale;
 
 import static android.content.ContentValues.TAG;
 
-public class SearchActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
-    Button search_buses;
-    EditText selectDate;
-    final Calendar c = Calendar.getInstance();
+public class SearchActivity extends AppCompatActivity {
+    private static final String LOG_TAG = SearchActivity.class.getSimpleName();
+
+    private EditText dateEditText;
+
+    private DatePickerListener datePickerListener = new DatePickerListener();
+    final private Calendar c = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +31,7 @@ public class SearchActivity extends AppCompatActivity implements DatePickerDialo
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_search);
-        search_buses = findViewById(R.id.search_buses);
+        Button search_buses = findViewById(R.id.search_buses);
         search_buses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,25 +39,32 @@ public class SearchActivity extends AppCompatActivity implements DatePickerDialo
                 startActivity(intent);
             }
         });
-        selectDate = findViewById(R.id.select_date);
-        selectDate.setOnClickListener(new View.OnClickListener() {
+
+        dateEditText = findViewById(R.id.select_date);
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                DatePickerFragment datePickerFragment = new DatePickerFragment();
-                datePickerFragment.show(getSupportFragmentManager(), "DatePicker");
+                Log.d(LOG_TAG, "Datepicker selected");
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                new DatePickerDialog(SearchActivity.this, datePickerListener, year, month, day).show();
             }
         });
 
     }
 
-    @Override
-    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, month);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        String selectedDate = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).format(c.getTime());
+    private class DatePickerListener implements DatePickerDialog.OnDateSetListener {
 
-        Log.d(TAG, "onDateSet: " + selectedDate);
-        // send date back to the target fragment
+        @Override
+        public void onDateSet(android.widget.DatePicker view, int year, int month, int dayOfMonth) {
+            c.set(Calendar.YEAR, year);
+            c.set(Calendar.MONTH, month);
+            c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            String selectedDate = new SimpleDateFormat("MM/dd/yyyy", Locale.ENGLISH).format(c.getTime());
 
+            Log.d(TAG, "onDateSet: " + selectedDate);
+            dateEditText.setText(selectedDate);
+        }
     }
 }
