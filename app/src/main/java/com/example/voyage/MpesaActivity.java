0d7449016bbcfbd18e7089ad0287d6f841d377e5
SamChess <38308778+SamChess@users.noreply.github.com>
2019-06-27 20:55:38 +0300

    Feature/bookings (#14)
    * more ui changes
    
    * adding more ui changes

--- app/src/main/java/com/example/voyage/MpesaActivity.java
+++ app/src/main/java/com/example/voyage/MpesaActivity.java
@@ -1,13 +1,28 @@
 package com.example.voyage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.voyage.ui.bookings.RecentBookingActivity;
 
 public class MpesaActivity extends AppCompatActivity {
+    Button pay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mpesa);
+
+         pay= findViewById(R.id.pay_button);
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RecentBookingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
