39c62dd3d4e2614e10aaaceac7467355d0e06f26
unknown <fcbsammymulwa@gmail.com>
2019-06-27 15:04:41 +0300

    more ui changes

---
+++ app/src/main/java/com/example/voyage/ui/bookings/RecentBookingActivity.java
@@ -0,0 +1,18 @@
+package com.example.voyage.ui.bookings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+import com.example.voyage.R;
+
+public class RecentBookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recent_booking);
+        recyclerView = (RecyclerView) findViewById(R.id.);
+
+    }
+}
